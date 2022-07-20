97387e67f227ef0901129fad24357c73d79a0569
Yannick wong <w-yannick@hotmail.fr>
2022-07-20 12:47:06 -0400

    Add first version of drawIO DB

---
+++ src/main/java/com/sg/cardealership/controller/ControllerExceptionHandler.java
@@ -0,0 +1,35 @@
+/**
+ * @author Yannick Wong
+ * email: w-yannick@hotmail.fr
+ * date: 14/07/2022
+ * purpose: Handler for the exception from the controller regarding SQL
+ */
+
+package com.sg.cardealership.controller;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final String CONSTRAINT_MESSAGE = "Could not save your item. "
+            + "Please ensure it is valid and try again.";
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public final ResponseEntity<Error> handleSqlException(
+            SQLIntegrityConstraintViolationException ex,
+            WebRequest request) {
+
+        Error err = new Error();
+        err.setMessage(CONSTRAINT_MESSAGE);
+        return new ResponseEntity<>(err, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+}
