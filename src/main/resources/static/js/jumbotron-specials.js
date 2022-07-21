963761d3bdac0b41f6b5b685260ca59d778ee123
Yannick wong <w-yannick@hotmail.fr>
2022-07-21 18:33:14 -0400

    Add fade in and fade out animation for jumbotron specials

--- src/main/resources/static/js/jumbotron-specials.js
+++ src/main/resources/static/js/jumbotron-specials.js
@@ -1,11 +1,14 @@
 var specials;
 var specialIndex = 0;
-
+var elem = $("#jumbotron-text");
 function changeText(){
-    $('#jumbotron-text').html(specials[specialIndex].description);
-    specialIndex =(specialIndex<( specials.length - 1))? ++specialIndex : 0;
+    $('#jumbotron-text').fadeOut(function(){
+        $('#jumbotron-text').html(specials[specialIndex].description);
+        specialIndex =(specialIndex<( specials.length - 1))? ++specialIndex : 0;
+        $('#jumbotron-text').fadeIn();
+    })
 };
-setInterval(changeText, 3000);
+setInterval(changeText, 5000);
 
 const userAction = async () => {
     const response = await fetch('http://localhost:8080/api/specials');
