aabec224947082c96d7733d0d38b51914e649111
Yannick wong <w-yannick@hotmail.fr>
2022-07-23 01:53:07 -0400

    Add dropdown for admin

--- src/main/resources/static/js/checkUser.js
+++ src/main/resources/static/js/checkUser.js
@@ -16,10 +16,29 @@ $(document).ready(function(){
             $('#navbar').append(
                 '"<li class="nav-item" th:class="${activePage == "sales"} ? "nav-item active" : "nav-item"><a class="nav-link" href="/sales">Sales</a></li>'
                 + '<li class="nav-item" th:class="${activePage == "admin"} ? "nav-item active" : "nav-item" ><a class="nav-link" href="/admin">Admin</a></li>'
-            );
+            
+                +'<li class="nav-item dropdown" th:class="${activePage == "admin"} ? "nav-item active" : "nav-item" >'
+                +'<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">'
+                +    'Admin'
+                +'</a>'
+                +'<div class="dropdown-menu" aria-labelledby="navbarDropdown">'
+                +  '<a class="dropdown-item" href="admin/vehicules">vehicules</a>'
+                +  '<div class="dropdown-divider"></div>'
+                +  '<a class="dropdown-item" href="/admin/users">users</a>' 
+                +  '<div class="dropdown-divider"></div>'
+                +  '<a class="dropdown-item" href="/admin/makes">makes</a>' 
+                +  '<div class="dropdown-divider"></div>'
+                +  '<a class="dropdown-item" href="/admin/models">models</a>' 
+                +  '<div class="dropdown-divider"></div>'
+                +  '<a class="dropdown-item" href="/admin/specials">reports</a>' 
+                +  '</div>'
+                + '</li>'   
+                
+                    );
             $('#logout').append(
                 '<li class="nav-item" th:class="${activePage == "login"} ? "nav-item active" : "nav-item" ><a class="nav-link" href="/login">logout</a></li>'
             );
+            
         
     }
      
